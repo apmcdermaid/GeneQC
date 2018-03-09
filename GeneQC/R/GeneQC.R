e1f4db7cd942cb958e25b0c546fa9358d506e516
apmcdermaid <36492156+apmcdermaid@users.noreply.github.com>
2018-03-09 14:44:13 -0600

    Update GeneQC.R

--- GeneQC/R/GeneQC.R
+++ GeneQC/R/GeneQC.R
@@ -81,7 +81,7 @@ GeneQC <- function(dat){
   set.seed(123456)
   B <-list()
   for (KK in ran){
-    B[[KK]] <- mixtools::normalmixEM(M,k=KK)
+    B[[KK]] <- mixtools::normalmixEM(M,k=KK, ECM = TRUE)
   }
 
   n <- length(M)   # number of observations
